790aa7dc55f515bae7c5f1e8cff7cedc57eb5f58
marvel51 <matias.szuchman@gmail.com>
2012-06-11 11:05:52 +0300

    Update master

--- ex9.cpp
+++ ex9.cpp
@@ -1,7 +1,21 @@
 #include "BtM.h"
+#DEFINE MAX 300
+#DEFINE MIN 100
 
+class set{
+    set(){new boolean array[max-min+1]=false};
+	set(int n){new boolean array[max-min+1]=false; array[MIN+n]=TRUE};
+boolean insert(int n){array[MIN+n]=TRUE}
+boolean remove(int n){array[MIN+n]=FALSE}
+boolean member(int n){return(array[MIN+n])}
+void print(imprime el array);
+	operator+,-,*;
+}
 
-//LALA
+set::set{
+    
+}
+
 main ()                                // test the functions
 {
     cout << "El que lee es un puto";
